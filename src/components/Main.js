2d318650f9aaa6a2a7f4d1f32f56658021c76222
jasenwyatt <jasen.wyatt@gmail.com>
2024-10-03 12:01:05 -0400

    create cleanup; initial component scaffolding;

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+  render() {
+    return (
+      <main>
+        <h1>Homepage</h1>
+      </main>
+    )
+  }
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href>Home</a></li>
+          <li><a href>About</a></li>
+          <li><a href>Menu</a></li>
+          <li><a href>Reserve</a></li>
+          <li><a href>Order</a></li>
+          <li><a href>Login</a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
